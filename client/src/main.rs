687feb40a20c5dc91a2dd98513e9927b195e2d3e
earomc <moritz.brunne@gmail.com>
2024-03-26 18:48:04 +0100

    Add bulk measuring for graphql queries

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,57 +1,108 @@
+mod num_util;
 mod queries;
 
 use std::{
     error::Error,
-    io::{stdin, Read},
+    fmt::Debug,
+    io::Read,
     time::{Duration, Instant},
 };
 
+use num_util::{average, median};
 use queries::*;
 use reqwest::blocking::{Client, Request, Response};
-use serde_json::{json, Value};
 
-const GRAPHQL_ENDPOINT: &str = "http://localhost:8080/graphql";
+pub const GRAPHQL_ENDPOINT: &str = "http://localhost:8080/graphql";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let client = reqwest::blocking::Client::builder()
         .timeout(Duration::from_secs(10))
         .build()?;
 
-    let graphql_query = r#"
-        query {
-            products(priceFilter: { min: 10, max: 100 }) {
-                id
-                name
-                price
-            }
-        }
-    "#;
+    let graphql_queries = get_graphql_queries();
 
-    let request = build_graphql_request(&client, graphql_query);
-    let mut result = measure_request(&client, request)?;
-    println!("{:?}", result.0);
-    let mut string = String::new();
-    result.1.read_to_string(&mut string)?;
-    println!("{}", string);
+    let bulk_result =
+        measure_graphql_request_bulk(&client, graphql_queries.get(0).unwrap(), 10000).unwrap();
+    
+    println!(
+        "Average {:?}, Median: {:?}, ",
+        bulk_result.average_duration, bulk_result.median_duration
+    );
     Ok(())
 }
 
-struct MeasureResult(Duration, Response);
+struct MeasureResult {
+    duration: Duration,
+    response: Response,
+    response_string: String,
+}
 
-fn build_graphql_request(client: &Client, query: &str) -> Request {
-    let body = json!({"query": query});
-
-    client
-        .post(GRAPHQL_ENDPOINT)
-        .header("Content-Type", "application/json")
-        .body(body.to_string())
-        .build()
-        .unwrap()
+impl Debug for MeasureResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "Duration: {:?}, Response: {:?}, Response as String: {}",
+            self.duration, self.response, self.response_string
+        ))
+    }
 }
 
 fn measure_request(client: &Client, request: Request) -> Result<MeasureResult, Box<dyn Error>> {
     let start_time = Instant::now();
-    let response = client.execute(request)?;
+    let mut response = client.execute(request)?;
     let elapsed_time = Instant::now().duration_since(start_time);
-    Ok(MeasureResult(elapsed_time, response))
+
+    let mut response_string = String::new();
+    response.read_to_string(&mut response_string).unwrap();
+
+    Ok(MeasureResult {
+        duration: elapsed_time,
+        response,
+        response_string,
+    })
+}
+
+struct BulkMeasureResult {
+    average_duration: Duration,
+    median_duration: Duration,
+    single_results: Vec<MeasureResult>,
+}
+
+impl Debug for BulkMeasureResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "Average duration: {:?}, Median duration: {:?}, Individual Results {:?}",
+            self.average_duration, self.median_duration, self.single_results
+        ))
+    }
+}
+
+impl BulkMeasureResult {
+    pub fn from_single_results(single_results: Vec<MeasureResult>) -> BulkMeasureResult {
+        let mut durations_as_nanos: Vec<u128> = single_results
+            .iter()
+            .map(|res| res.duration.as_nanos())
+            .collect();
+
+        let average_nanos = average(&durations_as_nanos);
+        let average_duration = Duration::from_nanos(average_nanos as u64);
+
+        let median_nanos = median(&mut durations_as_nanos);
+        let median_duration = Duration::from_nanos(median_nanos as u64);
+
+        Self { single_results, average_duration, median_duration }
+    }
+}
+
+fn measure_graphql_request_bulk(
+    client: &Client,
+    query: &str,
+    iterations: usize,
+) -> Result<BulkMeasureResult, Box<dyn Error>> {
+    let mut single_results = Vec::new();
+    for _ in 0..iterations {
+        let result = measure_request(client, build_graphql_request(&client, query))?;
+        single_results.push(result);
+    }
+
+    Ok(BulkMeasureResult::from_single_results(single_results))
 }
--- client/src/queries.rs
+++ client/src/queries.rs
@@ -1,17 +1,68 @@
-pub const REST_QUERIES: &[&'static str] = &["chinese government", "boo!"];
-pub const GRAPHQL_QUERIES: &[&'static str] = &["chinese government", "boo!"];
+use reqwest::blocking::{Client, Request};
+use serde_json::json;
 
-pub fn get_rest_queries() {
-    let queries: &[&str] = &[
+use crate::GRAPHQL_ENDPOINT;
+
+pub fn get_rest_requests() {
+    let _queries: &[&str] = &[
         "http://localhost:8080/products?min=1.0&max=50.0",
         "http://localhost:8080/products",
         "http://localhost:8080/orders",
-        "http://localhost:8080/customers"
-        ];
+        "http://localhost:8080/customers",
+    ];
 }
 
-pub fn get_graphql_queries() {
-    let queries: &[&str] = &[
-        "http://localhost:8080/graphql"
-        ];
+pub fn get_graphql_queries() -> Vec<String> {
+    let mut requests = Vec::new();
+
+    requests.push(
+        r#"
+        query {
+            products(priceFilter: { min: 10, max: 100 }) {
+                id
+                name
+                price
+            }
+        }
+        "#.into()
+    );
+
+    requests.push(
+        r#"
+        query allProducts {
+            products {
+                id
+                name
+                price
+                description
+            }
+        }
+        "#.into()
+    );
+
+    requests.push(
+        r#"
+        query allProducts {
+            products {
+                id
+                name
+                price
+                description
+            }
+        }
+        "#.into()
+    );
+
+    requests
+}
+
+pub fn build_graphql_request(client: &Client, query: &str) -> Request {
+    let body = json!({"query": query});
+
+    client
+        .post(GRAPHQL_ENDPOINT)
+        .header("Content-Type", "application/json")
+        .body(body.to_string())
+        .build()
+        .unwrap()
 }
