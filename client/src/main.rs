1d439086ff55dcaa73260911091900f8e404af3a
earomc <moritz.brunne@gmail.com>
2024-04-03 15:38:41 +0200

    add path names

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,7 @@ mod queries;
 mod measure;
 mod write_results;
 
-use std::{error::Error, time::Duration};
+use std::{env, error::Error, time::Duration};
 use queries::{get_graphql_queries, get_rest_queries};
 use measure::{measure_graphql_request_bulk, measure_rest_request_bulk};
 
@@ -11,11 +11,13 @@ pub const HOST_NAME: &str = "http://localhost:8080";
 pub const GRAPHQL_ENDPOINT: &str = "http://localhost:8080/graphql";
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env::set_var("RUST_BACKTRACE", "1");
+    
     let client = reqwest::blocking::Client::builder()
         .timeout(Duration::from_secs(10))
         .build()?;
 
-    let iterations = 100;
+    let iterations = 1000;
 
     let graphql_queries = get_graphql_queries();
     let rest_queries = get_rest_queries();
@@ -25,12 +27,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rest_query0 = rest_queries.get(0).unwrap();
     let bulk_result_rest = measure_rest_request_bulk(&client, &rest_query0.0, &rest_query0.1, iterations, "rest-0")?;
 
-    
-    let graphql_results = &vec![bulk_result_gql];
-    let rest_results = &vec![bulk_result_rest];
 
-    write_results::write_bulk_measure_results(graphql_results, "graphql")?;
-    write_results::write_bulk_measure_results(rest_results, "rest")?;
+    let graphql_results = vec![bulk_result_gql];
+    let rest_results = vec![bulk_result_rest];
+
+    write_results::write_bulk_measure_results(&graphql_results, "graphql")?;
+    write_results::write_bulk_measure_results(&rest_results, "rest")?;
+
+    write_results::write_results(&graphql_results.get(0).unwrap().single_results, "graphql_allcustomers")?;
+    write_results::write_results(&rest_results.get(0).unwrap().single_results, "rest_allcustomers")?;
 
     println!("GraphQL Result {:?}", graphql_results.get(0).unwrap());
     println!("Rest Result {:?}", rest_results.get(0).unwrap());
--- client/src/write_results.rs
+++ client/src/write_results.rs
@@ -1,39 +1,62 @@
-use std::{error::Error, path::Path};
+use std::{
+    error::Error, fs, path::PathBuf
+};
 
+use chrono::Utc;
 use csv::Writer;
 
 use crate::measure::{BulkMeasureResult, MeasureResult};
 
-pub fn write_results(results: &Vec<MeasureResult>, name: &str) -> Result<(), Box<dyn Error>> {
-    let path = name.to_string() + "-results.csv";
-    let path = Path::new(&path);
+fn create_path(is_bulk: bool, name: &str) -> PathBuf {
+    let mut path = PathBuf::new();
+    path.push("results");
+    fs::create_dir_all(&path).expect("couldn't create dirs");
+    path.push(format!(
+        "{}-{}{}-results.csv",
+        Utc::now().format("%d-%m-%Y_%H-%M"),
+        name.to_string(),
+        if is_bulk { "-bulk" } else { "" }
+    ));
+    path
+}
 
-    let mut wtr = Writer::from_path(path)?;
-    if !path.exists() {
-        wtr.write_record(&["duration_us", ])?;
+
+pub fn write_results(results: &Vec<MeasureResult>, name: &str) -> Result<(), Box<dyn Error>> {
+    let path = create_path(false, name);
+
+    let path_exists = path.try_exists().unwrap_or(false);
+    let mut wtr = Writer::from_path(&path)?;
+    if !path_exists {
+        wtr.write_record(&["duration_us"])?;
     }
     for result in results {
-        wtr.write_record(&[result.duration.as_micros().to_string(), ])?;
+        wtr.write_record(&[result.duration.as_micros().to_string()])?;
     }
 
     wtr.flush()?;
     Ok(())
 }
 
-pub fn write_bulk_measure_results(results: &Vec<BulkMeasureResult>, name: &str) -> Result<(), Box<dyn Error>> {
-    let path = name.to_string() + "-bulk-results.csv";
-    let path = Path::new(&path);
-    
-    let exists = path.try_exists().unwrap();
+pub fn write_bulk_measure_results<'a>(
+    results: impl IntoIterator<Item = &'a BulkMeasureResult<'a>>,
+    name: &str,
+) -> Result<(), Box<dyn Error>> {
+    let path = create_path(true, name);
+
+    let path_exists = path.try_exists().unwrap_or(false);
     let mut wtr = Writer::from_path(path)?;
-    if !exists {
+    if !path_exists {
         println!("Path didnt exist! Writing header.");
         wtr.write_record(&["name", "average_us", "median_us"])?;
     } else {
         println!("Path existed");
     }
     for result in results {
-        wtr.write_record(&[result.name.to_string(), result.average_duration.as_micros().to_string(), result.median_duration.as_micros().to_string()])?;
+        wtr.write_record(&[
+            result.name.to_string(),
+            result.average_duration.as_micros().to_string(),
+            result.median_duration.as_micros().to_string(),
+        ])?;
     }
     Ok(())
 }
